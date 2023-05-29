106f0cf032169234b0ec2148bd132ee7e4f782ff
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-05-29 14:20:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,583 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
-import {
-  EtherspotBatches,
-  EtherspotBatch,
-  EtherspotTransaction,
-  useEtherspotUi,
-  useEtherspotAddresses,
-  EtherspotContractTransaction,
-} from '@etherspot/transaction-kit';
+import "./App.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { RainbowKitProvider, connectorsForWallets, lightTheme } from "@rainbow-me/rainbowkit";
+import { metaMaskWallet, injectedWallet, rainbowWallet, walletConnectWallet } from "@rainbow-me/rainbowkit/wallets";
+import { polygonMumbai, optimismGoerli, goerli, gnosisChiado } from "@wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { BananaWallet } from "@rize-labs/banana-rainbowkit-plugin";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { CssBaseline } from "@mui/material";
+import Layout from "./components/Layout";
+import NoMatch from "./pages/NoMatch";
+import Home from "./pages/Home";
 
-
-const { estimate, send } = useEtherspotTransactions();
-const etherspotAddresses = useEtherspotAddresses();
-
-const [address, setAddress] = useState('0x271Ae6E03257264F0F7cb03506b12A027Ec53B31');
-const [amount, setAmount] = useState('0.001');
-
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "mint",
+        async lazy() {
+          let Mint = await import("./pages/Mint");
+          return { Component: Mint.default };
+        },
+      },
+      {
+        path: "event/:id",
+        async lazy() {
+          let Event = await import("./pages/Event");
+          return { Component: Event.default };
+        },
+        children: [
+          {
+            path: ":id",
+            async lazy() {
+              let Event = await import("./pages/Event");
+              return { Component: Event.default };
+            },
+          },
+        ],
+      },
+      {
+        path: "events",
+        async lazy() {
+          let Events = await import("./pages/Events");
+          return { Component: Events.default };
+        },
+      },
+      {
+        path: "*",
+        element: <NoMatch />,
+      },
+    ],
+  },
+]);
 
 function App() {
+  const { chains, provider } = configureChains(
+    // currently on these three chains are supported by BananaWallet
+    [gnosisChiado],
+    [publicProvider()]
+  );
+
+  const connectors = connectorsForWallets([
+    {
+      groupName: "Recommended",
+      wallets: [
+        BananaWallet({ chains, connect: { networkId: 80001 } }),
+        metaMaskWallet({ chains, shimDisconnect: true }),
+        rainbowWallet({ chains }),
+        walletConnectWallet({ chains }),
+        injectedWallet({ chains, shimDisconnect: true }),
+      ],
+    },
+  ]);
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  });
+
   return (
-    <EtherspotBatches onEstimated={onEstimateReceiver}>
-  <EtherspotBatch>
-    
-    <EtherspotContractTransaction
-      contractAddress={'0x9DC78b651F640244211c792a703fceD2c41F16f8'}
-      abi={[
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "_parentId",
-              "type": "uint256"
-            },
-            {
-              "internalType": "string",
-              "name": "_name",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "_image",
-              "type": "string"
-            }
-          ],
-          "name": "addChildEvent",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "stateMutability": "nonpayable",
-          "type": "constructor"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "owner",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "approved",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "Approval",
-          "type": "event"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "owner",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "operator",
-              "type": "address"
-            },
-            {
-              "indexed": false,
-              "internalType": "bool",
-              "name": "approved",
-              "type": "bool"
-            }
-          ],
-          "name": "ApprovalForAll",
-          "type": "event"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "approve",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "string",
-              "name": "_name",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "_image",
-              "type": "string"
-            }
-          ],
-          "name": "createEvent",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "from",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "safeTransferFrom",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "from",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            },
-            {
-              "internalType": "bytes",
-              "name": "data",
-              "type": "bytes"
-            }
-          ],
-          "name": "safeTransferFrom",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "operator",
-              "type": "address"
-            },
-            {
-              "internalType": "bool",
-              "name": "approved",
-              "type": "bool"
-            }
-          ],
-          "name": "setApprovalForAll",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "_childEventId",
-              "type": "uint256"
-            },
-            {
-              "internalType": "uint256",
-              "name": "_parentId",
-              "type": "uint256"
-            },
-            {
-              "internalType": "string",
-              "name": "_image",
-              "type": "string"
-            }
-          ],
-          "name": "setChildEventImage",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "_eventId",
-              "type": "uint256"
-            },
-            {
-              "internalType": "string",
-              "name": "_image",
-              "type": "string"
-            }
-          ],
-          "name": "setEventImage",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "from",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "Transfer",
-          "type": "event"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "from",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "transferFrom",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "owner",
-              "type": "address"
-            }
-          ],
-          "name": "balanceOf",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "name": "events",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "name",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "image",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "getApproved",
-          "outputs": [
-            {
-              "internalType": "address",
-              "name": "",
-              "type": "address"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "_childEventId",
-              "type": "uint256"
-            }
-          ],
-          "name": "getChildEventImage",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "_parentId",
-              "type": "uint256"
-            }
-          ],
-          "name": "getChildEvents",
-          "outputs": [
-            {
-              "internalType": "uint256[]",
-              "name": "",
-              "type": "uint256[]"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "_eventId",
-              "type": "uint256"
-            }
-          ],
-          "name": "getEventImage",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "_childEventId",
-              "type": "uint256"
-            }
-          ],
-          "name": "getParentEventId",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "owner",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "operator",
-              "type": "address"
-            }
-          ],
-          "name": "isApprovedForAll",
-          "outputs": [
-            {
-              "internalType": "bool",
-              "name": "",
-              "type": "bool"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "name",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "ownerOf",
-          "outputs": [
-            {
-              "internalType": "address",
-              "name": "",
-              "type": "address"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "bytes4",
-              "name": "interfaceId",
-              "type": "bytes4"
-            }
-          ],
-          "name": "supportsInterface",
-          "outputs": [
-            {
-              "internalType": "bool",
-              "name": "",
-              "type": "bool"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "symbol",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "tokenURI",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "totalEvents",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        }
-      ]}
-      methodName={'createEvent'}
-      params={['X', 'https://ipfs.io/ipfs/bafybeicankr7uxlxbll6b3ohwi2dfnjyluq6w4epuohquebub47gcpqp5u/8953.jpg']}
-      value={'0'}
-    />
-
-
-   
-  </EtherspotBatch>
-</EtherspotBatches>
+    <div className="App">
+      <CssBaseline />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          chains={chains}
+          coolMode
+          theme={lightTheme({
+            accentColor: "#623485", //color of wallet  try #703844
+            accentColorForeground: "white", //color of text
+            borderRadius: "large", //rounded edges
+            fontStack: "system",
+          })}
+        >
+          <RouterProvider router={router} fallbackElement={<p>Loading...</p>}></RouterProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
+      <ToastContainer />
+    </div>
   );
 }
 

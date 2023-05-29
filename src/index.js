b4a35d0ebd6189befadc542ed124458d94d27638
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-05-29 14:18:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { EtherspotTransactionKit } from '@etherspot/transaction-kit';
-import { ethers } from 'ethers';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const randomWallet = ethers.Wallet.createRandom();
-const providerWallet = new ethers.Wallet(randomWallet.privateKey);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <EtherspotTransactionKit
-    provider={providerWallet} /* The random wallet we created above */
-    chainId={80001} /* Polygon Testnet - Mumbai */
-  >
     <App />
-  </EtherspotTransactionKit>
-</React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
